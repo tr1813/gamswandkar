bcfcd1fe03053feba7b3f9c5b5bd5111b1c9c705
tr1813 <tang.mich.root@gmail.com>
2021-07-25 10:19:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gamswandkar Cave Survey Project
+
+## What is this?
+
+This repository hosts cave data and drawings of the caves of the Gamswandkar area [47.790°N 13.681°E 47.795°N 13.686°E].
+
+
+
+
+## How to contribute
+
+## 
---
+++ thTMPDIR/data.pl
@@ -0,0 +1,25 @@
+%SCRAP = (
+	# scrap: marco_polo_schacht-1p
+	aaaaaaa => {
+		 F => "data.1",
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 E => "data.3",
+		 Y => 1,
+		 V => -1,
+		 S => "40.91 -44.41",
+	},
+);
+
+
+%LAYER = (
+	# expanded map: m-all-p
+	1 => {
+		N => 'm-all-p',
+	},
+);
+
+
+
+$PATTERN_DEFS = "./";
